41db6a000aab6773dfc52b7e57e01f8afd4b750b
Ali Emre SAVAŞ <aliemresavas.95@gmail.com>
2020-02-16 16:03:32 +0000

    Introduction section -> Solution 5 is added.

---
+++ Python/Introduction/Python Loops/solution.py
@@ -0,0 +1,9 @@
+"""
+    author : Ali Emre SAVAS
+    Link : https://www.hackerrank.com/challenges/python-loops/problem
+"""
+if __name__ == '__main__':
+    n = int(input())
+    if(n >= 1 and n <= 20):
+        for i in range(0, n):
+            print(i**2)
