a13f1a7e2236ac78f192966ea54f1975afe04e77
Ali Emre SAVAŞ <aliemresavas.95@gmail.com>
2020-02-16 20:08:26 +0000

    Basic Data Types section -> Solution 3 is added.

---
+++ Python/Basic Data Types/Nested List/solution.py
@@ -0,0 +1,25 @@
+"""
+    author : Ali Emre SAVAS
+    Link :
+"""
+if __name__ == '__main__':
+    students = list()
+    # This part to take input for list
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name, score])
+    # List is sorted according to score.
+    students.sort(key=lambda x: x[1])
+    lowestScore = students[0][1]
+    # This part to find second lowest score.
+    for name, score in students:
+        if score > lowestScore:
+            secondLowestScore = score
+            break
+    # This filter part to find same score.
+    printingList = list(filter(lambda x: x[1] == secondLowestScore, students))
+    # Then, I sorted list according to names.
+    printingList.sort(key=lambda x: x[0])
+    for name, score in printingList:
+        print(name)
