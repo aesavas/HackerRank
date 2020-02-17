e07a6e5357eb309282598041a6c4845854687bea
Ali Emre SAVAŞ <aliemresavas.95@gmail.com>
2020-02-17 19:39:49 +0000

    30 Days of Code -> Day 01 is added.

---
+++ 30 Days of Code/Day 01 - Data Types/solution.py
@@ -0,0 +1,18 @@
+"""
+    auhtor : Ali Emre SAVAS
+    Link : https://www.hackerrank.com/challenges/30-data-types/problem
+"""
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+intNumber = int(input())
+floatNumber = float(input())
+sentece = input()
+
+result = i + intNumber
+print(result)
+result = d + floatNumber
+print(result)
+result = s + sentece
+print(result)
