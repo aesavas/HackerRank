3839f26b90d3608ec628133f24ec4f2e7cb0835a
Ali Emre SAVAŞ <aliemresavas.95@gmail.com>
2020-02-19 23:45:12 +0000

    30 Days of Code -> Day 03 is added.

---
+++ 30 Days of Code/Day 03 - Introduction to Conditional Statements/solution.py
@@ -0,0 +1,15 @@
+"""
+    author : Ali Emre SAVAS
+    Link : https://www.hackerrank.com/challenges/30-conditional-statements/problem
+"""
+if __name__ == '__main__':
+    n = int(input())
+    if(n % 2 == 0):
+        if(n >= 2 and n <= 5):
+            print("Not Weird")
+        elif(n >= 6 and n <= 20):
+            print("Weird")
+        else:
+            print("Not Weird")
+    else:
+        print("Weird")
