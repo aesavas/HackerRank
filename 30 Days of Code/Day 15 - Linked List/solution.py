b231120a26970a227efd0eb23ab3a4c735ea8e84
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2020-07-05 14:34:49 +0100

    30 Days of Code -> Day 15 is added.

---
+++ 30 Days of Code/Day 15 - Linked List/solution.py
@@ -0,0 +1,33 @@
+"""
+    author : Ali Emre SAVAS
+    Link : https://www.hackerrank.com/challenges/30-linked-list/problem
+"""
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+        if(head == None):
+            return Node(data)
+        else:
+            node = head
+            while(node.next != None):
+                node = node.next
+            node.next = Node(data)
+            return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
