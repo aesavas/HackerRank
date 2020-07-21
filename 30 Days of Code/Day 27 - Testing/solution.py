32576f27c264f083632ecebae021f06fda41b52c
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2020-07-22 00:30:57 +0100

    30 Days of Code -> Day 27 is added.

---
+++ 30 Days of Code/Day 27 - Testing/solution.py
@@ -0,0 +1,79 @@
+"""
+    author : Ali Emre SAVAS
+    Link : https://www.hackerrank.com/challenges/30-testing/problem
+"""
+
+def minimum_index(seq):
+    if len(seq) == 0:
+        raise ValueError("Cannot get the minimum value index from an empty sequence")
+    min_idx = 0
+    for i in range(1, len(seq)):
+        if seq[i] < seq[min_idx]:
+            min_idx = i
+    return min_idx
+class TestDataEmptyArray(object):
+    
+    @staticmethod
+    def get_array():
+        emptyList = list()
+        return emptyList
+
+class TestDataUniqueValues(object):
+
+    @staticmethod
+    def get_array():
+        uniqueList = [7,5,3,9]
+        return uniqueList
+        
+
+    @staticmethod
+    def get_expected_result():
+        return 2
+
+class TestDataExactlyTwoDifferentMinimums(object):
+
+    @staticmethod
+    def get_array():
+        twoDifferentMinimum = [7,5,9,3,6,3]
+        return twoDifferentMinimum
+
+    @staticmethod
+    def get_expected_result():
+        return 3
+
+def TestWithEmptyArray():
+    try:
+        seq = TestDataEmptyArray.get_array()
+        result = minimum_index(seq)
+    except ValueError as e:
+        pass
+    else:
+        assert False
+
+
+def TestWithUniqueValues():
+    seq = TestDataUniqueValues.get_array()
+    assert len(seq) >= 2
+
+    assert len(list(set(seq))) == len(seq)
+
+    expected_result = TestDataUniqueValues.get_expected_result()
+    result = minimum_index(seq)
+    assert result == expected_result
+
+
+def TestiWithExactyTwoDifferentMinimums():
+    seq = TestDataExactlyTwoDifferentMinimums.get_array()
+    assert len(seq) >= 2
+    tmp = sorted(seq)
+    assert tmp[0] == tmp[1] and (len(tmp) == 2 or tmp[1] < tmp[2])
+
+    expected_result = TestDataExactlyTwoDifferentMinimums.get_expected_result()
+    result = minimum_index(seq)
+    assert result == expected_result
+
+TestWithEmptyArray()
+TestWithUniqueValues()
+TestiWithExactyTwoDifferentMinimums()
+print("OK")
+
