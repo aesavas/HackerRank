a76727e8d20c075a62ba4fa5b2f4df7580787eaf
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2020-09-28 23:39:30 +0100

    BigNumber section -> Solution 3 is added.

---
+++ Java/BigNumber/Java BigInteger/Solution.java
@@ -0,0 +1,20 @@
+/*
+    author : Ali Emre SAVAS
+    Link : https://www.hackerrank.com/challenges/java-biginteger/problem
+    ------------------------
+*/
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BigInteger A = scan.nextBigInteger();
+        BigInteger B = scan.nextBigInteger();
+        System.out.println(A.add(B));
+        System.out.println(A.multiply(B));
+        
+    }
+}
